1614cd084e2e8325d17865ce6d497fcdaec25798
Kyei Cundy <nabcoasfeg14756@gmail.com>
2024-07-28 17:08:37 +0100

    alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS Customers(
  );
  
 
-CREATE TABLE IF NOT EXISTS Order(
+CREATE TABLE IF NOT EXISTS Orders(
     order_id  INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
@@ -42,7 +42,7 @@ CREATE TABLE IF NOT EXISTS Order_Details(
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREING KEY(order_id) REFERENCES Orders(order_id)
+    FOREING KEY(order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
     
